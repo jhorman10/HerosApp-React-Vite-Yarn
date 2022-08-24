688a02412deed25592f8cdaa1e806f9da81e6b7c
Jhorman Orozco <jsojhor@gmail.com>
2022-08-24 01:07:14 -0500

    add styles in SearchPage

--- src/heros/pages/SearchPage/index.jsx
+++ src/heros/pages/SearchPage/index.jsx
@@ -22,10 +22,14 @@ export const SearchPage = () => {
 
   const showAlertMessage = () => {
     if (q === '') {
-      return <div className="alert alert-primary">Search a hero</div>;
+      return (
+        <div className="alert alert-primary animate__animated animate__fadeIn">
+          Search a hero
+        </div>
+      );
     } else if (heros.length === 0) {
       return (
-        <div className="alert alert-danger">
+        <div className="alert alert-danger animate__animated animate__fadeIn">
           No hero with <b>{q}</b>
         </div>
       );
